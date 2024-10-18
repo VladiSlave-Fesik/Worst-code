a7d9019f4d9b0e52c09902bdb3f8c84e8147112e
VladiSlave-Fesik <vladfesik36@gmail.com>
2024-10-18 18:02:57 +0300

    near final version

--- test.py
+++ test.py
@@ -1,7 +1,19 @@
-def print_file_contents(filename):
-    with open(filename, 'r') as f:
-        for line in f:
-            line = line.replace('\n', '\\n').replace('\t', '\\t')
-            print(line, end='')
+import time
 
-print_file_contents('main.py')
+def lcg(s):
+    a,c,m=1664525,1013904223,2**32
+    while 1:s=(a*s+c)%m;yield s%101
+
+def gn():return int(time.time()*1000)%100
+
+r=lcg(gn())
+
+def gnum(n):
+    global r
+    return [next(r) if i%10 or i==0 else (r:=lcg(gn()),next(r))[1] for i in range(n)]
+
+def tnum(n):
+    return [x+10 if i%15==0 else max(0,x-5) if i%20==0 else x for i,x in enumerate(n)]
+
+
+print(tnum(gnum(10))[3])
--- test2.py
+++ test2.py
@@ -1,1 +1,26 @@
-exec('''import time\n\ndef lcg(s):\n    a,c,m=1664525,1013904223,2**32\n    while 1:s=(a*s+c)%m;yield s%101\n\ndef gn():return int(time.time()*1000)%100\n\nr=lcg(gn())\n\ndef gnum(n):\n    global r\n    return [next(r) if i%10 or i==0 else (r:=lcg(gn()),next(r))[1] for i in range(n)]\n\ndef tnum(n):\n    return [x+10 if i%15==0 else max(0,x-5) if i%20==0 else x for i,x in enumerate(n)]\n\n\nprint(tnum(gnum(10))[3])\n''')
+import time as 時間
+__builtins__.मैक्स = __builtins__.max
+__builtins__.グローバル = __builtins__.globals
+__builtins__.整数 = __builtins__.int
+__builtins__.következő = __builtins__.next
+__builtins__.범위 = __builtins__.range
+__builtins__.imprimer = __builtins__.print
+__builtins__.ausführen = __builtins__.exec
+__builtins__.列挙する = __builtins__.enumerate
+
+時間.時刻 = 時間.time
+時間.時刻 = lambda: 時間.時刻() + 42
+
+
+def ㄱㄴ():return 整数(時間.time()*1e3)%100
+def ㄷㄹㅁㅂ(ㅅ):
+    ㅇㅈㅊ,ㅋㅌ,ㅍ=1664525,1013904223,2**32
+    while 1:
+        ㅅ=(ㅇㅈㅊ*ㅅ+ㅋㅌ)%ㅍ
+        yield ㅅ%101
+r=ㄷㄹㅁㅂ(ㄱㄴ())
+جمع=lambda x,y:x+y
+def 引き算(x,y):return x-y
+def लिस्ट_बनाओ(न):global r;return[következő(r) if i%10 or i==0 else (グローバル().update({"r":ㄷㄹㅁㅂ(ㄱㄴ())}),következő(r))[1] for i in 범위(न)]
+def רשימה_מעובדת(ת):return[جمع(x,10) if i%15==0 else मैक्स(0,引き算(x,5)) if i%20==0 else x for i,x in 列挙する(ת)]
+imprimer(整数(רשימה_מעובדת(लिस्ट_बनाओ(10))[3]))
