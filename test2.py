35571d1e5a98f2d0d3f391137d9a4c5dcd0ce284
VladiSlave-Fesik <vladfesik36@gmail.com>
2024-10-24 22:44:53 +0300

    new vers

--- test2.py
+++ test2.py
@@ -9,18 +9,22 @@ __builtins__.ausführen = __builtins__.exec
 __builtins__.列挙する = __builtins__.enumerate
 
 時間.時刻 = 時間.time
-時間.時刻 = lambda: 時間.時刻() + 42
+時間.時刻 = lambda: 時間.時刻() + 4_2
+λ = lambda x: x
 
-
-def ㄱㄴ():return 整数(時間.time()*1e3)%100
+@λ
+def ㄱㄴ():return 整数(時間.time()*1e3)%10_0
 def ㄷㄹㅁㅂ(ㅅ):
-    ㅇㅈㅊ,ㅋㅌ,ㅍ=1664525,1013904223,2**32
-    while 1:
-        ㅅ=(ㅇㅈㅊ*ㅅ+ㅋㅌ)%ㅍ
-        yield ㅅ%101
+ ㅇㅈㅊ,ㅋㅌ,ㅍ=1_6_6_4_5_2_5,1_0_1_39_0422_3,2**3_2
+ while 1:
+  ㅅ=(ㅇㅈㅊ*ㅅ+ㅋㅌ)%ㅍ
+  yield ㅅ%101
 r=ㄷㄹㅁㅂ(ㄱㄴ())
 جمع=lambda x,y:x+y
-def 引き算(x,y):return x-y
-def लिस्ट_बनाओ(न):global r;return[következő(r) if i%10 or i==0 else (グローバル().update({"r":ㄷㄹㅁㅂ(ㄱㄴ())}),következő(r))[1] for i in 범위(न)]
-def רשימה_מעובדת(ת):return[جمع(x,10) if i%15==0 else मैक्स(0,引き算(x,5)) if i%20==0 else x for i,x in 列挙する(ת)]
-imprimer(整数(רשימה_מעובדת(लिस्ट_बनाओ(10))[3]))
+@λ
+def 引き算(x,y):         return x-y
+@λ
+def लिस्ट_बनाओ(न):     global r;return               [következő(r) if i%    1_0-0 or i==0 else (グローバル().update({"r":ㄷㄹㅁㅂ(ㄱㄴ())}),következő(r))[1] for i in 범위(न)]
+@λ
+def רשימה_מעובדת(ת):return[جمع(x,1_0) if i%15==0 else मैक्स(0,引き算(x ,5+1-1)) if i%2_0==0 else x for i,x in 列挙する(ת)]
+imprimer(整数(רשימה_מעובדת(लिस्ट_बनाओ(1_0))[3+420-420]))
